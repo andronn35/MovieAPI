171cfb85481517c9a1324b45309338bea97acae0
andronn35 <mr.andron35@rambler.ru>
2021-08-19 19:05:03 +0300

    mobile adaptation

--- src/components/Header/HeaderMovies.js
+++ src/components/Header/HeaderMovies.js
@@ -25,28 +25,32 @@ const HeaderMovies = () => {
 
   return (
     <div className={classes.header}>
-      <div>Movies</div>
+      <div className={classes.logo}>Movies</div>
       
       <div className={classes.pagination}>
-        Show by &nbsp;
-        {limitArr.map((l, index) => {
-          return (
-            <span  className={limit === l && classes.selectedPage} 
-              key={index}
-              onClick={() => dispatch(setLimit(l))}
-            >{" " + l}</span>
-          )
-        })} &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-        {pages.map((p, index) => {
-          return (
-            <span className={currentPage === p && classes.selectedPage}
-              key={index}              
-              onClick={() => dispatch(setCurrentPage(p))}
-            >
-              {" " + p}
-            </span>
-          )
-        })}
+        <div>
+          Show by &nbsp;
+          {limitArr.map((l, index) => {
+            return (
+              <span  className={limit === l && classes.selectedPage} 
+                key={index}
+                onClick={() => dispatch(setLimit(l))}
+              >{" " + l}</span>
+            )
+          })} &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        </div>
+        <div>
+          {pages.map((p, index) => {
+            return (
+              <span className={currentPage === p && classes.selectedPage}
+                key={index}              
+                onClick={() => dispatch(setCurrentPage(p))}
+              >
+                {" " + p}
+              </span>
+            )
+          })}
+        </div>
       </div>
     </div>
   )
